cf01397217ec9342207e47dbfe7a4f2a0e524b15
Vadim Kleiman <vadim.kleiman@gmail.com>
2017-09-09 11:12:16 +0300

    Implemented StringSet

---
+++ hw01/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace hw01
+{
+    class MainClass
+    {
+        public static void Main(string[] args) 
+        {
+            IStringSet stringSet = new StringSetImpl();
+            stringSet.Add("abc");
+            stringSet.Add("abcc");
+            stringSet.Remove("abc");
+            Console.WriteLine(stringSet.Contains("abc"));
+            Console.WriteLine(stringSet.Size());
+            Console.WriteLine(stringSet.HowManyStartsWithPrefix("abc"));
+        }
+    }
+}
