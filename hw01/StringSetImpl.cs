438b653c09c197022db5f293fd48cf6aa642d82a
Vadim Kleiman <vadim.kleiman@gmail.com>
2017-09-21 23:30:11 +0300

    some fix

--- hw01/StringSetImpl.cs
+++ hw01/StringSetImpl.cs
@@ -1,106 +1,106 @@
 ﻿namespace hw01
 {
-    public class StringSetImpl : IStringSet
-    {
-        class Trie
-        {
-            public bool isTerminated;
-            public int howManyStartsWithPrefix;
-            public Trie[] nodes = new Trie[SYMBOL_COUNT];
-        }
+	public class StringSetImpl : IStringSet
+	{
+		class Trie
+		{
+			public bool isTerminated;
+			public int howManyStartsWithPrefix;
+			public Trie[] nodes = new Trie[SYMBOL_COUNT];
+		}
 
-        static int START_INDEX = 26;
-        static int SYMBOL_COUNT = 52;
-        readonly Trie root;
+		static int START_INDEX = 26;
+		static int SYMBOL_COUNT = 52;
+		readonly Trie root;
 
-        public StringSetImpl()
-        {
-            root = new Trie();
-        }
+		public StringSetImpl()
+		{
+			root = new Trie();
+		}
 
-        public bool Add(string element)
-        {
-            if (Contains(element))
-            {
-                return false;
-            }
-            Trie node = root;
-            for (int i = 0; i < element.Length; i++)
-            {
-                int index = GetIndex(element[i]);
-                if (node.nodes[index] == null)
-                {
-                    node.nodes[index] = new Trie();
-                }
-                node = node.nodes[index];
-                node.howManyStartsWithPrefix++;
-            }
+		public bool Add(string element)
+		{
+			if (Contains(element))
+			{
+				return false;
+			}
+			Trie node = root;
+			foreach (var ch in element)
+			{
+				int index = GetIndex(ch);
+				if (node.nodes[index] == null)
+				{
+					node.nodes[index] = new Trie();
+				}
+				node = node.nodes[index];
+				node.howManyStartsWithPrefix++;
+			}
 
-            node.isTerminated = true;
-            root.howManyStartsWithPrefix++;
-            return true;
-        }
+			node.isTerminated = true;
+			root.howManyStartsWithPrefix++;
+			return true;
+		}
 
-        public bool Contains(string element)
-        {
-            Trie node = Traverse(element);
-            return node != null && node.isTerminated;
-        }
+		public bool Contains(string element)
+		{
+			Trie node = Traverse(element);
+			return node != null && node.isTerminated;
+		}
 
-        public int HowManyStartsWithPrefix(string prefix)
-        {
-            Trie node = Traverse(prefix);
-            return node == null ? 0 : node.howManyStartsWithPrefix;
-        }
+		public int HowManyStartsWithPrefix(string prefix)
+		{
+			Trie node = Traverse(prefix);
+			return node == null ? 0 : node.howManyStartsWithPrefix;
+		}
 
-        public bool Remove(string element)
-        {
-            if (!Contains(element))
-            {
-                return false;
-            }
-            Trie current = root;
-            for (int i = 0; i < element.Length; i++)
-            {
-                current.howManyStartsWithPrefix--;
-                Trie prev = current;
-                current = current.nodes[GetIndex(element[i])];
-                if (current.howManyStartsWithPrefix == 1)
-                {
-                    prev.nodes[GetIndex(element[i])] = null;
-                }
-            }
-            current.howManyStartsWithPrefix--;
-            current.isTerminated = false;
-            return true;
-        }
+		public bool Remove(string element)
+		{
+			if (!Contains(element))
+			{
+				return false;
+			}
+			Trie current = root;
+			foreach (var ch in element)
+			{
+				current.howManyStartsWithPrefix--;
+				Trie prev = current;
+				current = current.nodes[GetIndex(ch)];
+				if (current.howManyStartsWithPrefix == 1)
+				{
+					prev.nodes[GetIndex(ch)] = null;
+				}
+			}
+			current.howManyStartsWithPrefix--;
+			current.isTerminated = false;
+			return true;
+		}
 
-        public int Size()
-        {
-            return root.howManyStartsWithPrefix;
-        }
+		public int Size()
+		{
+			return root.howManyStartsWithPrefix;
+		}
 
-        static int GetIndex(char symbol)
-        {
-            if (symbol >= 'a' && symbol <= 'z')
-            {
-                return START_INDEX + symbol - 'a';
-            }
-            return symbol - 'A';
-        }
+		static int GetIndex(char symbol)
+		{
+			if (symbol >= 'a' && symbol <= 'z')
+			{
+				return START_INDEX + symbol - 'a';
+			}
+			return symbol - 'A';
+		}
 
-        Trie Traverse(string element)
-        {
-            Trie node = root;
-            for (int i = 0; i < element.Length; i++)
-            {
-                if (node == null)
-                {
-                    return null;
-                }
-                node = node.nodes[GetIndex(element[i])];
-            }
-            return node;
-        }
-    }
+		Trie Traverse(string element)
+		{
+			Trie node = root;
+			foreach (var ch in element)
+			{
+				if (node == null)
+				{
+					return null;
+				}
+				node = node.nodes[GetIndex(ch)];
+			}
+			return node;
+		}
+	}
 }
