958efe91b188135413b2d3b45e9100a19a99ccc7
Vadim Kleiman <vadim.kleiman@gmail.com>
2017-09-30 23:23:21 +0300

    Implement Option

---
+++ Option/Option.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Option
+{
+    public sealed class Option<T>
+	{
+		static public Option<T> Some(T value) => new Option<T>(value, true);
+
+		static public Option<T> None() => new Option<T>(default(T), false);
+
+        static public Option<T> Flatten(Option<Option<T>> o) => o.Value;
+
+		public bool IsNone { get => !IsSome; }
+
+		public bool IsSome { get => isValue; }
+
+		public T Value
+		{
+			get
+			{
+				if (IsNone)
+				{
+					throw new Exception();
+				}
+				return value;
+			}
+		}
+
+		public Option<U> Map<U>(Func<T, U> func) => IsNone ?
+			Option<U>.None() : Option<U>.Some(func(value));
+
+        public override int GetHashCode() => 
+            value.GetHashCode() ^ isValue.GetHashCode();
+
+        public override bool Equals(object obj)
+        {
+            if ((object)this == obj)
+            {
+                return true;
+            }
+            var other = obj as Option<T>;
+            if ((object)other == null)
+            {
+                return false;
+            }
+            if (IsNone == other.IsNone == true)
+            {
+                return true;
+            }
+            return other.Value.Equals(value);
+        }
+
+		private Option(T val, bool isNone)
+		{
+			value = val;
+			isValue = isNone;
+		}
+
+        private readonly T value;
+
+        private readonly bool isValue;
+	}
+}
---
+++ OptionTest/UnitTest1.cs
@@ -0,0 +1,42 @@
+using System;
+using Xunit;
+
+namespace OptionTest
+{
+    public class UnitTest1
+    {
+
+        [Fact]
+        public void SimpleTest()
+        {
+            const int check = 2;
+            var o = Option.Option<int>.Some(check);
+            Assert.Equal(check, o.Value);
+        }
+
+        [Fact]
+        public void MapTest()
+        {
+            const int check = 4;
+            Assert.Equal(Option.Option<int>
+                .Some(check).Map(x => x * x), Option.Option<int>.Some(16));
+        }
+
+        [Fact]
+        public void FlattenTest()
+        {
+            const int check = 1;
+            var a = Option.Option<int>.Some(check);
+            var b = Option.Option<Option.Option<int>>.Some(a);
+            var c = Option.Option<int>.Flatten(b);
+            Assert.Equal(c, a);
+        }
+
+        [Fact]
+        public void EmptyTest()
+        {
+            Assert.Equal(Option.Option<int>.None().Map(x => x),
+                         Option.Option<int>.None());
+        }
+    }
+}
