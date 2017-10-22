4e86c2ccd70dc5248993866d4d030d5c8979c910
Vadim Kleiman <vadim.kleiman@gmail.com>
2017-10-22 12:05:43 +0300

    code style fix

--- Option/Option.cs
+++ Option/Option.cs
@@ -3,33 +3,33 @@
 namespace Option
 {
     public sealed class Option<T>
-	{
-		static public Option<T> Some(T value) => new Option<T>(value, true);
+    {
+        static public Option<T> Some(T value) => new Option<T>(value, true);
 
-		static public Option<T> None() => new Option<T>(default(T), false);
+        static public Option<T> None() => new Option<T>(default(T), false);
 
         static public Option<T> Flatten(Option<Option<T>> o) => o.Value;
 
-		public bool IsNone { get => !IsSome; }
+        public bool IsNone { get => !IsSome; }
 
-		public bool IsSome { get => isValue; }
+        public bool IsSome { get => isValue; }
 
-		public T Value
-		{
-			get
-			{
-				if (IsNone)
-				{
-					throw new Exception();
-				}
-				return value;
-			}
-		}
+        public T Value
+        {
+            get
+            {
+                if (IsNone)
+                {
+                    throw new Exception();
+                }
+                return value;
+            }
+        }
 
-		public Option<U> Map<U>(Func<T, U> func) => IsNone ?
-			Option<U>.None() : Option<U>.Some(func(value));
+        public Option<U> Map<U>(Func<T, U> func) => IsNone ?
+            Option<U>.None() : Option<U>.Some(func(value));
 
-        public override int GetHashCode() => 
+        public override int GetHashCode() =>
             value.GetHashCode() ^ isValue.GetHashCode();
 
         public override bool Equals(object obj)
@@ -50,14 +50,14 @@ namespace Option
             return other.Value.Equals(value);
         }
 
-		private Option(T val, bool isNone)
-		{
-			value = val;
-			isValue = isNone;
-		}
+        private Option(T val, bool isNone)
+        {
+            value = val;
+            isValue = isNone;
+        }
 
         private readonly T value;
 
         private readonly bool isValue;
-	}
+    }
 }
