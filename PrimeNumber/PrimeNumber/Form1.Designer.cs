dc004dfa5c6044678cd6937a44ee7c6badedf49b
Vadim Kleiman <vadim.kleiman@gmail.com>
2017-11-12 14:05:21 +0300

    Impl Prime Number

---
+++ PrimeNumber/PrimeNumber/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace PrimeNumber
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(138, 7);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 36);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(320, 367);
+            this.flowLayoutPanel1.TabIndex = 2;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(12, 10);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            1000000000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(320, 403);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.button1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Prime";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+    }
+}
+
---
+++ PrimeNumber/PrimeNumber/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Timer = System.Windows.Forms.Timer;
+
+namespace PrimeNumber
+{
+    public partial class Form1 : Form
+    {
+        private readonly Timer _timer = new Timer();
+        private readonly List<DataControl> _futures = new List<DataControl>();
+
+        private sealed class DataControl
+        {
+            public Task CurrentTask { get; }
+            public Label Status { get; }
+            public Label Result { get; }
+            public ProgressBar Progress { get; }
+            public Button Cancel { get; }
+            public Tuple Data { get; } 
+
+            public DataControl(Task task, Label status, Label result, ProgressBar progress, Button cancel, Tuple data)
+            {
+                CurrentTask = task;
+                Status = status;
+                Result = result;
+                Progress = progress;
+                Cancel = cancel;
+                Data = data;
+            }
+        }
+
+        private sealed class Tuple
+        {
+            public int CurrentProgress { get; set; }
+            public CancellationTokenSource CancelTokenSource { get; }
+            public CancellationToken Token { get; }
+            public int Result { get; set; }
+
+            public Tuple(int currentProgress, CancellationToken token, CancellationTokenSource cancelTokenSource)
+            {
+                CurrentProgress = currentProgress;
+                Token = token;
+                CancelTokenSource = cancelTokenSource;
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            _timer.Interval = 10;
+            _timer.Tick += timer_Tick;
+            _timer.Start();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            foreach (var f in _futures)
+            {
+                if (f.CurrentTask.Status == TaskStatus.Running)
+                {
+                    f.Cancel.Visible = f.Progress.Visible = true;
+                    f.Progress.Value = f.Data.CurrentProgress;
+                    f.Status.Text = f.CurrentTask.Status.ToString();
+                }
+                else
+                {
+                    f.Cancel.Visible = f.Progress.Visible = false;
+                }
+                if (f.Data.Token.IsCancellationRequested)
+                {
+                    f.Status.Text = @"Cancel";
+                }
+                else if (f.CurrentTask.IsCompleted)
+                {
+                    f.Result.Text = f.Data.Result.ToString();
+                    f.Result.Visible = true;
+                    f.Status.Text = @"Finish";
+                }
+                else if (f.CurrentTask.Status == TaskStatus.WaitingToRun)
+                {
+                    f.Status.Text = @"Waiting";
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var value = Convert.ToInt32(numericUpDown1.Text);
+            var cancelTokenSource = new CancellationTokenSource();
+            var token = cancelTokenSource.Token;
+            var data = new Tuple(0, token, cancelTokenSource);
+            var status = new Label() {Width = 50};
+            var result = new Label() {Width = 50, Visible = false};
+            var progress = new ProgressBar() {Minimum = 0, Maximum = value, Width = 130, Visible = false};
+            var cancel = new Button() {Text = @"Cancel", Visible = false};
+            cancel.Click += (ss, ee) => { cancelTokenSource.Cancel(); };
+            var panel = new FlowLayoutPanel() {Height = 30, Width = 300};
+            panel.Controls.Add(status);
+            panel.Controls.Add(result);
+            panel.Controls.Add(progress);
+            panel.Controls.Add(cancel);
+            Controls["flowLayoutPanel1"].Controls.Add(panel);
+            var task = new Task(() =>
+            {
+                var count = 0;
+                for (var i = 1; i <= value; i++)
+                {
+                    var isPrime = true;
+                    for (var j = 2; j <= Math.Sqrt(i); ++j)
+                    {
+                        if (token.IsCancellationRequested)
+                        {
+                            return;
+                        }
+                        if (i % j != 0) continue;
+                        isPrime = false;
+                        break;
+                    }
+                    if (isPrime)
+                    {
+                        ++count;
+                    }
+                    ++data.CurrentProgress;
+                }
+                data.Result = count;
+            });
+            task.Start();
+            _futures.Add(new DataControl(task, status, result, progress, cancel, data));
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            foreach (var f in _futures)
+            {
+                f.Data.CancelTokenSource.Cancel();
+            }
+        }
+    }
+}
